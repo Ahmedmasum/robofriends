5c59ce39ea729b5a86ef3cb6a3e8955ed03fc232
Ahmedmasum <64851526+Ahmedmasum@users.noreply.github.com>
2020-05-23 02:40:16 +0600

    Updates

---
+++ precache-manifest.bb8548fb8a3872bf77e4f99cfefb3d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973bc92b43467755ed50f4fe9a3e672c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "38ebc82c22a53faee233",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "0123e3549c3deff5b234",
+    "url": "/robofriends/static/css/main.1a6fcc51.chunk.css"
+  },
+  {
+    "revision": "38ebc82c22a53faee233",
+    "url": "/robofriends/static/js/2.1deaa823.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.1deaa823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0123e3549c3deff5b234",
+    "url": "/robofriends/static/js/main.1e8354a0.chunk.js"
+  },
+  {
+    "revision": "a8edc8fd40b167dbac71",
+    "url": "/robofriends/static/js/runtime-main.22e8467b.js"
+  }
+]);
